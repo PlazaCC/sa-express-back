07c847301971c6885de16e7c3f37e29a1f9d3cca
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-09 17:07:34 -0300

    updating o create_user.py

--- src/routes/create_user.py
+++ src/routes/create_user.py
@@ -1,4 +1,3 @@
-from curses import OK
 from datetime import time
 import uuid
 from src.shared.domain.entities.user import User
@@ -6,13 +5,13 @@ from src.shared.domain.enums.role_enum import ROLE
 from src.shared.domain.enums.user_status_enum import USER_STATUS
 from src.shared.helpers.errors.errors import EntityError, ForbiddenAction, MissingParameters
 from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
-from src.shared.helpers.external_interfaces.http_codes import BadRequest, Forbidden, InternalServerError
+from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, Forbidden, InternalServerError
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 from src.shared.infra.repositories.dtos.auth_authorizer_dto import AuthAuthorizerDTO
 from src.shared.infra.repositories.repository import Repository
 
 
-class  Controller:
+class Controller:
     @staticmethod
     def execute(request: IRequest) -> IResponse:
         try:
@@ -37,9 +36,9 @@ class  Controller:
                 name=request.data.get('name'),
                 email=request.data.get('email'),
                 phone=request.data.get('phone'),
-                role=request.data.get('role')
+                role=ROLE[request.data.get('role')]
             )
-            return OK(body=response)\
+            return OK(body=response)
             
         except MissingParameters as error:
             return BadRequest(error.message)
@@ -56,11 +55,11 @@ class Usecase:
     
     def __init__(self):
         self.repository = Repository(auth_repo=True)
-        self.repository = self.repository.auth_repo
+        self.auth_repo = self.repository.auth_repo
         
     def execute(self, name: str, email: str, phone: str, role: ROLE):
         user = User(
-            user_id=uuid.uuid4(), 
+            user_id=str(uuid.uuid4()), 
             name=name,
             email=email,
             phone=phone,
@@ -72,13 +71,18 @@ class Usecase:
             enabled=True
         )
         
-        user_created = self.repository.create_user(user)
+        user_created = self.auth_repo.create_user(
+            email=email, 
+            name=name, 
+            role=role
+        )
         return user_created.to_dict()
     
-    def function_handler(event, context):
-        http_request = LambdaHttpRequest(event)
-        http_request.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
-        response = Controller.execute(http_request)
-        http_response = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
-        
-        return http_response.toDict()
+
+def function_handler(event, context):
+    http_request = LambdaHttpRequest(event)
+    http_request.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
+    response = Controller.execute(http_request)
+    http_response = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    
+    return http_response.toDict()
