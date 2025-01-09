af24a1d66ff8a316cf9dd60210fd0b67419aeb8c
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-08 22:42:06 -0300

    add support for authentication repository in the Repository class

--- src/shared/infra/repositories/repository.py
+++ src/shared/infra/repositories/repository.py
@@ -1,5 +1,6 @@
-from src.environments import STAGE, Environments
+from src.shared.environments import STAGE, Environments
 from src.shared.domain.repositories.deal_repository_interface import IDealRepository
+from src.shared.infra.repositories.auth.auth_repository_cognito import AuthRepositoryCognito
 from src.shared.infra.repositories.database.deal_repository_dynamo import DealRepositoryDynamo
 from src.shared.infra.repositories.mocks.deal_repository_mock import DealRepositoryMock
 
@@ -10,6 +11,7 @@ class Repository:
     def __init__(
             self,
             deal_repo: bool = False,
+            auth_repo: bool = False
     ):
         self.session = None
 
@@ -19,13 +21,16 @@ class Repository:
             )
         else:
             self._initialize_database_repositories(
-                deal_repo
+                deal_repo,
+                auth_repo
             )
 
     def _initialize_mock_repositories(self, deal_repo):
         if deal_repo:
             self.deal_repo = DealRepositoryMock()
         
-    def _initialize_database_repositories(self, deal_repo):
+    def _initialize_database_repositories(self, deal_repo, auth_repo):
         if deal_repo:
             self.deal_repo = DealRepositoryDynamo()
+        if auth_repo:
+            self.auth_repo = AuthRepositoryCognito()
