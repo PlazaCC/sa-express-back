2021c3887359f1d66322f70bd7ac769c5603a855
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-01-21 11:53:01 -0300

    feat: Adicionar métodos de criação e desativação de perfil na interface do repositório

--- src/shared/domain/repositories/profile_repository_interface.py
+++ src/shared/domain/repositories/profile_repository_interface.py
@@ -7,5 +7,13 @@ class IProfileRepository(ABC):
     @abstractmethod
     def get_profile_by_id(self, user_id: str) -> Profile:
         pass
+    
+    @abstractmethod
+    def create_profile(self, profile: Profile) -> Profile:
+        pass
+    
+    @abstractmethod
+    def deactivate_profile(self, user_id: str) -> Profile:
+        pass
 
   
