3453f738e55e5471dc6cf14f76f8aff863d35535
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-01-20 21:31:27 -0300

    Add profile repository initialization to the Repository class

--- src/shared/infra/repositories/repository.py
+++ src/shared/infra/repositories/repository.py
@@ -2,7 +2,9 @@ from src.shared.environments import STAGE, Environments
 from src.shared.domain.repositories.deal_repository_interface import IDealRepository
 from src.shared.infra.repositories.auth.auth_repository_cognito import AuthRepositoryCognito
 from src.shared.infra.repositories.database.deal_repository_dynamo import DealRepositoryDynamo
+from src.shared.infra.repositories.database.profile_repository_dynamo import ProfileRepositoryDynamo
 from src.shared.infra.repositories.mocks.deal_repository_mock import DealRepositoryMock
+from src.shared.infra.repositories.mocks.profile_repository_mock import ProfileRepositoryMock
 
 
 class Repository:
@@ -11,24 +13,32 @@ class Repository:
     def __init__(
             self,
             deal_repo: bool = False,
-            auth_repo: bool = False
+            auth_repo: bool = False,
+            profile_repo: bool = False
     ):
         if Environments.stage == STAGE.TEST:
             self._initialize_mock_repositories(
-                 deal_repo
+                 deal_repo,
+                 profile_repo
             )
         else:
             self._initialize_database_repositories(
                 deal_repo,
-                auth_repo
+                auth_repo,
+                profile_repo
             )
+            
 
-    def _initialize_mock_repositories(self, deal_repo):
+    def _initialize_mock_repositories(self, deal_repo, profile_repo):
         if deal_repo:
             self.deal_repo = DealRepositoryMock()
+        if profile_repo:
+            self.profile_repo = ProfileRepositoryMock()
         
-    def _initialize_database_repositories(self, deal_repo, auth_repo):
+    def _initialize_database_repositories(self, deal_repo, auth_repo, profile_repo):
         if deal_repo:
             self.deal_repo = DealRepositoryDynamo()
         if auth_repo:
             self.auth_repo = AuthRepositoryCognito()
+        if profile_repo:
+            self.profile_repo = ProfileRepositoryDynamo()
