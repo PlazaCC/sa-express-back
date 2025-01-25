e14d222cd326f685f88ba7a07533da63d136bf68
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-01-25 03:48:41 -0300

    feat: Adicionar campos user_id e deal_id à entidade Affiliation e implementar validação de UUID

--- src/shared/domain/entities/affiliation.py
+++ src/shared/domain/entities/affiliation.py
@@ -1,25 +1,31 @@
 from typing import Optional
+
+from pydantic import Field, field_validator
 from src.shared.helpers.errors.errors import EntityError
 
 
 class Affiliation:
     def __init__(
         self,
-        affiliation_id: str,
-        invoiced_amount: float,
-        amount_to_pay: float,
-        commission: float,
-        baseline: float,
-        cost_per_acquisition: int,
-        rev_share: float,
-        registers: int,
-        ftd: int,
-        shipping_volume: float,
-        base_quality: float,
-        operators_satisfaction: float,
+        affiliation_id: str = Field(..., description="String with 32 characters"),
+        user_id: str = Field(..., description="String with 32 characters"),
+        deal_id: str = Field(..., description="String with 32 characters"),
+        invoiced_amount: float = Field(..., description="Float"),
+        amount_to_pay: float = Field(..., description="Float"),
+        commission: float = Field(..., description="Float"),
+        baseline: float = Field(..., description="Float"),
+        cost_per_acquisition: int = Field(..., description="Integer"),
+        rev_share: float = Field(..., description="Float"),
+        registers: int = Field(..., description="Integer"),
+        ftd: int = Field(..., description="Integer"),
+        shipping_volume: float = Field(..., description="Float"),
+        base_quality: float = Field(..., description="Float"),
+        operators_satisfaction: float = Field(..., description="Float"),
     ):
         self._validate_affiliation(
             affiliation_id,
+            user_id,
+            deal_id,
             invoiced_amount,
             amount_to_pay,
             commission,
@@ -34,6 +40,8 @@ class Affiliation:
         )
 
         self.affiliation_id = affiliation_id
+        self.user_id = user_id
+        self.deal_id = deal_id
         self.invoiced_amount = invoiced_amount
         self.amount_to_pay = amount_to_pay
         self.commission = commission
@@ -46,6 +54,15 @@ class Affiliation:
         self.base_quality = base_quality
         self.operators_satisfaction = operators_satisfaction
 
+    @field_validator('user_id', 'deal_id', 'affiliation_id')
+    @staticmethod
+    def validate_uuid(value: str) -> str:
+        if not isinstance(value, str):
+            raise EntityError
+        if len(value) != 36:
+            raise ValueError("uuids must have exactly 36 characters")
+        return value
+    
     @staticmethod
     def _validate_affiliation(
         affiliation_id: str,
@@ -89,17 +106,34 @@ class Affiliation:
             )
 
     def to_dict(self) -> dict:
-        return {
-            "affiliation_id": self.affiliation_id,
-            "invoiced_amount": self.invoiced_amount,
-            "amount_to_pay": self.amount_to_pay,
-            "commission": self.commission,
-            "baseline": self.baseline,
-            "cost_per_acquisition": self.cost_per_acquisition,
-            "rev_share": self.rev_share,
-            "registers": self.registers,
-            "ftd": self.ftd,
-            "shipping_volume": self.shipping_volume,
-            "base_quality": self.base_quality,
-            "operators_satisfaction": self.operators_satisfaction,
-        }
+      return {
+        "affiliation_id": self.affiliation_id,
+        "invoiced_amount": self.invoiced_amount,
+        "amount_to_pay": self.amount_to_pay,
+        "commission": self.commission,
+        "baseline": self.baseline,
+        "cost_per_acquisition": self.cost_per_acquisition,
+        "rev_share": self.rev_share,
+        "registers": self.registers,
+        "ftd": self.ftd,
+        "shipping_volume": self.shipping_volume,
+        "base_quality": self.base_quality,
+        "operators_satisfaction": self.operators_satisfaction,
+      }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'Affiliation':
+      return cls(
+        affiliation_id=data.get("affiliation_id"),
+        invoiced_amount=data.get("invoiced_amount"),
+        amount_to_pay=data.get("amount_to_pay"),
+        commission=data.get("commission"),
+        baseline=data.get("baseline"),
+        cost_per_acquisition=data.get("cost_per_acquisition"),
+        rev_share=data.get("rev_share"),
+        registers=data.get("registers"),
+        ftd=data.get("ftd"),
+        shipping_volume=data.get("shipping_volume"),
+        base_quality=data.get("base_quality"),
+        operators_satisfaction=data.get("operators_satisfaction"),
+      )
