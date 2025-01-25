b011ec9f7b4e3bca07e9426c73c4342ac2c2f7c7
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-01-25 03:49:14 -0300

    feat: Implementar repositório de entidades utilizando DynamoDB

---
+++ src/shared/infra/repositories/database/entity_repository_dynamo.py
@@ -0,0 +1,46 @@
+from src.shared.domain.entities.entity import Entity
+from src.shared.domain.repositories.entity_repository_interface import IEntityRepository
+from src.shared.environments import Environments
+from src.shared.infra.external.dynamo_datasource import DynamoDatasource
+
+
+class EntityRepositoryDynamo(IEntityRepository):
+  
+  @staticmethod
+  def entity_partition_key_format(entity_id: str) -> str:
+    return f'ENTITY#{entity_id}'
+  
+  @staticmethod
+  def entity_sort_key_format() -> str:
+    return f'METADATA'
+  
+  def __init__(self):
+    self.dynamo = DynamoDatasource(
+      dynamo_table_name=Environments.get_envs().dynamo_table_name,
+      region=Environments.get_envs().region,
+      partition_key=Environments.get_envs().dynamo_partition_key,
+      sort_key=Environments.get_envs().dynamo_sort_key,
+      gsi_partition_key=Environments.get_envs().dynamo_gsi_partition_key,
+    )
+    
+  def create_entity(self, entity: Entity):
+    try:
+      self.dynamo.put_item(
+        item=entity.to_dict(),
+        partition_key=self.entity_partition_key_format(entity.entity_id),
+        sort_key=self.entity_sort_key_format()
+      )
+      return entity
+    except Exception as e:
+      raise e
+    
+  def get_entity_by_id(self, entity_id: str):
+    try:
+      entity = self.dynamo.get_item(partition_key=self.entity_partition_key_format(entity_id))
+      
+      if "Item" not in entity:
+        return None
+      
+      return Entity.from_dict(entity['Item'])
+    except Exception as e:
+      raise e
