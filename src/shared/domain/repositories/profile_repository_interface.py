632956f19d62ef393d96d8861a9e0ee4f9078426
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-02-01 12:26:05 -0300

    refactor: adicionar métodos para gerenciar afiliações na interface do repositório de perfis

--- src/shared/domain/repositories/profile_repository_interface.py
+++ src/shared/domain/repositories/profile_repository_interface.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import List
 
+from src.shared.domain.entities.affiliation import Affiliation
 from src.shared.domain.entities.profile import Profile
 
 class IProfileRepository(ABC):
@@ -14,6 +16,18 @@ class IProfileRepository(ABC):
     
     @abstractmethod
     def deactivate_profile(self, user_id: str) -> Profile:
+        pass
+    
+    @abstractmethod
+    def get_affiliation_by_id(self, user_id: str) -> Affiliation:
+        pass
+    
+    @abstractmethod
+    def create_affiliation(self, affiliation: Affiliation) -> Affiliation:
+        pass
+    
+    @abstractmethod
+    def get_all_my_affiliations(self, user_id: str) -> List[Affiliation]:
         pass
 
   
