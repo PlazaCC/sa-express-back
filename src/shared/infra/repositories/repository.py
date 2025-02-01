edd7810446008d94853b89d74ef230aeb56868de
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-02-01 12:26:39 -0300

    refactor: remover repositório de afiliações da classe Repository

--- src/shared/infra/repositories/repository.py
+++ src/shared/infra/repositories/repository.py
@@ -1,13 +1,10 @@
-from src.shared.domain.repositories.affiliation_repository_interface import IAffiliationRepository
 from src.shared.domain.repositories.entity_repository_interface import IEntityRepository
 from src.shared.domain.repositories.profile_repository_interface import IProfileRepository
 from src.shared.environments import STAGE, Environments
 from src.shared.infra.external.dynamo_datasource import DynamoDatasource
 from src.shared.infra.repositories.auth.auth_repository_cognito import AuthRepositoryCognito
-from src.shared.infra.repositories.database.affiliation_repository_dynamo import AffiliationRepositoryDynamo
 from src.shared.infra.repositories.database.entity_repository_dynamo import EntityRepositoryDynamo
 from src.shared.infra.repositories.database.profile_repository_dynamo import ProfileRepositoryDynamo
-from src.shared.infra.repositories.mocks.affiliation_repository_mock import AffiliationRepositoryMock
 from src.shared.infra.repositories.mocks.entity_repository_mock import EntityRepositoryMock
 from src.shared.infra.repositories.mocks.profile_repository_mock import ProfileRepositoryMock
 
@@ -15,40 +12,34 @@ from src.shared.infra.repositories.mocks.profile_repository_mock import ProfileR
 class Repository:
     auth_repo: AuthRepositoryCognito
     profile_repo: IProfileRepository
-    affiliation_repo: IAffiliationRepository
     entity_repo: IEntityRepository
 
     def __init__(
             self,
             auth_repo: bool = False,
             profile_repo: bool = False,
-            affiliation_repo: bool = False,
             entity_repo: bool = False
     ):
         if Environments.stage == STAGE.TEST:
             self._initialize_mock_repositories(
                 profile_repo,
-                affiliation_repo,
                 entity_repo
             )
         else:
             self._initialize_database_repositories(
                 auth_repo,
                 profile_repo,
-                affiliation_repo,
                 entity_repo
             )
             
 
-    def _initialize_mock_repositories(self, profile_repo, affiliation_repo, entity_repo):
+    def _initialize_mock_repositories(self, profile_repo, entity_repo):
         if profile_repo:
             self.profile_repo = ProfileRepositoryMock()
-        if affiliation_repo:
-            self.affiliation_repo = AffiliationRepositoryMock()
         if entity_repo:
             self.entity_repo = EntityRepositoryMock()
             
-    def _initialize_database_repositories(self, auth_repo, profile_repo, affiliation_repo, entity_repo):
+    def _initialize_database_repositories(self, auth_repo, profile_repo, entity_repo):
         dynamo = DynamoDatasource(
             dynamo_table_name=Environments.dynamo_table_name,
             region=Environments.region,
@@ -59,7 +50,5 @@ class Repository:
             self.auth_repo = AuthRepositoryCognito()
         if profile_repo:
             self.profile_repo = ProfileRepositoryDynamo(dynamo)
-        if affiliation_repo:
-            self.affiliation_repo = AffiliationRepositoryDynamo(dynamo)
         if entity_repo:
             self.entity_repo = EntityRepositoryDynamo(dynamo)
