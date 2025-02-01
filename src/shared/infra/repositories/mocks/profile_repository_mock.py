528cc772d3ed4b6a73b76fa7c075af640e043f3b
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2025-02-01 12:26:46 -0300

    refactor: adicionar métodos para gerenciar afiliações no mock do repositório de perfis

--- src/shared/infra/repositories/mocks/profile_repository_mock.py
+++ src/shared/infra/repositories/mocks/profile_repository_mock.py
@@ -4,16 +4,17 @@ from src.shared.domain.entities.affiliation import Affiliation
 from src.shared.domain.enums.profile_status_enum import PROFILE_STATUS
 from src.shared.domain.enums.role_enum import ROLE
 from src.shared.domain.repositories.profile_repository_interface import IProfileRepository
+from src.shared.domain.entities.affiliation import Affiliation
 
 
 class ProfileRepositoryMock(IProfileRepository):
     profiles: List[Profile]
+    affiliations: List[Affiliation]
 
     def __init__(self):
         self.profiles = [
             Profile(
                 user_id="00000000-0000-0000-0000-000000000000",
-                entity_id="00000000-0000-0000-0000-000000000000",
                 game_data_id="00000000-0000-0000-0000-000000000000",
                 affiliations=[
                     "00000000-0000-0000-0000-000000000000",
@@ -25,7 +26,6 @@ class ProfileRepositoryMock(IProfileRepository):
             ),
             Profile(
                 user_id="00000000-0000-0000-0000-000000000001",
-                entity_id="00000000-0000-0000-0000-000000000001",
                 game_data_id="00000000-0000-0000-0000-000000000001",
                 affiliations=[
                     "00000000-0000-0000-0000-000000000001",
@@ -35,6 +35,56 @@ class ProfileRepositoryMock(IProfileRepository):
                 created_at=1620000000,
                 updated_at=1620000000,
             ),
+        ]
+        self.affiliations = [
+            Affiliation(
+                affiliation_id="00000000-0000-0000-0000-000000000000",
+                deal_id="00000000-0000-0000-0000-000000000000",
+                user_id="00000000-0000-0000-0000-000000000000",
+                base_quality=1,
+                amount_to_pay=1,
+                baseline=1,
+                commission=1,
+                cost_per_acquisition=1,
+                ftd=1,
+                invoiced_amount=1,
+                operators_satisfaction=1,
+                registers=1,
+                rev_share=1,
+                shipping_volume=1,
+            ),
+            Affiliation(
+                affiliation_id="00000000-0000-0000-0000-000000000001",
+                deal_id="00000000-0000-0000-0000-000000000001",
+                user_id="00000000-0000-0000-0000-000000000001",
+                amount_to_pay=2,
+                base_quality=2,
+                baseline=2,
+                commission=2,
+                cost_per_acquisition=2,
+                ftd=2,
+                invoiced_amount=2,
+                operators_satisfaction=2,
+                registers=2,
+                rev_share=2,
+                shipping_volume=2,
+            ),
+            Affiliation(
+                affiliation_id="00000000-0000-0000-0000-000000000002",
+                deal_id="00000000-0000-0000-0000-000000000002",
+                user_id="00000000-0000-0000-0000-000000000002",
+                amount_to_pay=3,
+                baseline=3,
+                commission=3,
+                base_quality=3,
+                cost_per_acquisition=3,
+                ftd=3,
+                invoiced_amount=3,
+                operators_satisfaction=3,
+                registers=3,
+                rev_share=3,
+                shipping_volume=3,
+            )
         ]
 
     def get_profile_by_id(self, user_id: str) -> Profile:
@@ -53,3 +103,22 @@ class ProfileRepositoryMock(IProfileRepository):
                 profile.status = PROFILE_STATUS.INACTIVE
                 return profile
         return None
+    
+    def get_affiliation_by_id(self, user_id: str) -> Affiliation:
+        for affiliation in self.affiliations:
+            if affiliation.user_id == user_id:
+                return affiliation
+        return None
+      
+    def get_all_my_affiliations(self, user_id: str) -> List[Affiliation]:
+        affiliations = []
+        
+        for affiliation in self.affiliations:
+         if affiliation.user_id == user_id:
+            affiliations.append(affiliation)
+            
+        return affiliations
+    
+    def create_affiliation(self, affiliation: Affiliation) -> Affiliation:
+        self.affiliations.append(affiliation)
+        return affiliation
